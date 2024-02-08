13432954859852565f45301c08737d637a6d703b
Samuel Steiger <samcsteiger@gmail.com>
2024-02-08 10:39:11 -0500

    Added tailwindcss to project

--- src/lib/headshot.svelte
+++ src/lib/headshot.svelte
@@ -1,1 +1,4 @@
-<img src="headshot.jpg" alt="Sam Steiger's Headshot" width="150" height="150"/>
+<div class="bg-indigo-300 flex justify-center">
+    <img class=""
+    src="headshot.jpg" alt="Sam Steiger's Headshot" width="150" height="150"/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,16 @@
 
 </script>
 
+<style lang="postcss">
+	:global(html) {
+		background-color: #00A366;
+		}
+
+</style>
+
 <Headshot/>
-<h1>Welcome to Sam's Personal Website</h1>
+<h1>
+	Welcome to Sam's Personal Website
+</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>Hello World, I hope you have a wonderful day!</p>
