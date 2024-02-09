ff16d342eeeb4011a1fa2d3dc1f789ab5549429e
Samuel Steiger <samcsteiger@gmail.com>
2024-02-09 09:22:47 -0500

    Made headshot circular

--- src/lib/headshot.svelte
+++ src/lib/headshot.svelte
@@ -1,4 +1,16 @@
-<div class="bg-indigo-300 flex justify-center">
-    <img class=""
-    src="headshot.jpg" alt="Sam Steiger's Headshot" width="150" height="150"/>
+<script>
+    const headshot_path = "headshot.jpg"
+    const headshot_desc = "Sam Steiger's Headshot"
+</script>
+
+<style>
+    img {
+        height: 150px;
+        width: 150px;
+        border-radius: 50%;
+    }
+</style>
+
+<div class="bg-indigo-300 flex justify-center py-3">
+    <img src={headshot_path} alt={headshot_desc}/>
 </div>
