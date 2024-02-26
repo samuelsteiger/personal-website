ffff51040bf275e47d62d8ebe1e2eb5518ab23b5
Samuel Steiger <samcsteiger@gmail.com>
2024-02-26 16:51:26 -0500

    Created WaterBottle component

---
+++ src/lib/water_bottle.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Headshot from "$lib/headshot.svelte";
+	import Bio from "$lib/bio.svelte";
+	import ProjectBanner from "$lib/project_banner.svelte";
+	import SkillSheet from "$lib/skill_sheet.svelte";
+</script>
+
+<style>
+    .lid {
+        background-color: black;
+        width: 100%;
+        height: 10vh;
+    }
+
+
+    .lid p {
+        color: white;
+        font-size: 25px;
+    }
+
+    .bottle {
+        background-color: #00A366;
+        width: 100%;
+        min-height: 90vh;
+    }
+    
+</style>
+
+<div class="lid grid grid-cols-3 gap-1 justify-evenly justify-items-center place-items-center">
+    <div><p>About Me</p></div>
+    <div><p>Projects</p></div>
+    <div><p>Blog</p> </div>
+</div>
+
+<div class="bottle">
+    <Headshot/>
+    <Bio/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,9 @@
 <script>
-	import Headshot from "$lib/headshot.svelte";
-	import Bio from "$lib/bio.svelte";
-	import ProjectBanner from "$lib/project_banner.svelte";
-	import Skill from "$lib/skill.svelte"
-	import SkillSheet from "$lib/skill_sheet.svelte";
+	import WaterBottle from "$lib/water_bottle.svelte";
 </script>
 
 <style lang="postcss">
-	:global(html) {
-		background-color: #00A366;
-		}
 
 </style>
 
-<Headshot/>
-<Bio/>
-<SkillSheet/>
-<ProjectBanner/>
-
+<WaterBottle/>
