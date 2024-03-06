8e87d8cd66ad5540ed460404b7174b8eb5a138b2
Samuel Steiger <samcsteiger@gmail.com>
2024-03-06 16:48:29 -0500

    Added grid structure to bottle

--- src/lib/bio.svelte
+++ src/lib/bio.svelte
@@ -8,6 +8,5 @@
 
 <div class="text-center py-3 text-3xl">
     <h1>Welcome to Sam's Personal Website</h1>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-    <p>Hello World, I hope you have a wonderful day!</p>
+    <p>I hope you have a wonderful day!</p>
 </div>
--- src/lib/water_bottle.svelte
+++ src/lib/water_bottle.svelte
@@ -21,18 +21,49 @@
     .bottle {
         background-color: #00A366;
         width: 100%;
-        min-height: 90vh;
+        min-height: 100vh;
+        
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: auto auto auto auto;
+        gap: 0px 0px;
+        grid-auto-flow: row;
+        grid-template-areas:
+            "Nav Nav"
+            "Bio Bio"
+            "Employment Skills"
+            "Projects Projects";
     }
-    
+
+    .Nav { grid-area: Nav; }
+
+    .Bio { grid-area: Bio; }
+
+    .Employment { grid-area: Employment; }
+
+    .Skills { grid-area: Skills; }
+
+    .Projects { grid-area: Projects; }
+
 </style>
 
-<div class="lid grid grid-cols-3 gap-1 justify-evenly justify-items-center place-items-center">
-    <div><p>About Me</p></div>
-    <div><p>Projects</p></div>
-    <div><p>Blog</p> </div>
-</div>
-
 <div class="bottle">
-    <Headshot/>
-    <Bio/>
+    <div class="Nav lid grid grid-cols-3 gap-1 justify-evenly justify-items-center place-items-center">
+        <div><p>About Me</p></div>
+        <div><p>Projects</p></div>
+        <div><p>Blog</p> </div>
+    </div>
+    <div class="Bio">
+        <Headshot/>
+        <Bio/>
+    </div>
+    <div class="Employment"></div>
+    <div class="Skills">
+        <SkillSheet/>
+    </div>
+    <div class="Projects">
+        <ProjectBanner/>
+        <ProjectBanner/>
+
+    </div>
 </div>
