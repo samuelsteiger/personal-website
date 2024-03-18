8ecdbd6983ccce8d06ce8e84b05673e8924c3007
Samuel Steiger <samcsteiger@gmail.com>
2024-03-18 14:46:00 -0400

    ported bio component

---
+++ client/src/bio.js
@@ -0,0 +1,16 @@
+import "./bio.css"
+
+export default function Bio() {
+    return( 
+    <>
+        <div class="bio-title">
+            <p class="name">Samuel Steiger</p>
+            <p class="title">SBU Graduate | Big Inquisitive Nerd | Aspiring SWE</p>
+        </div>
+        <div class="bio-text">
+            <p>Hi! My name is Samuel Steiger, I am an aspiring software engineer from Wading River, New York. I love to work on personal projects in the areas I’m interested in such as social media and market analysis to sharpen my skills and grow. My formal education is in Computer Science, which I had the pleasure of studying at Stony Brook University. I hope to get the opportunity to work on exciting new projects in the future and collaborate with smart and inventive people.</p>
+        </div>
+    </>
+    )
+       
+}
