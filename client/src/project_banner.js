20f0c30a510fc07b8449084c0b0bb1825efde955
Samuel Steiger <samcsteiger@gmail.com>
2024-03-20 14:28:58 -0400

    fixed the styling of banner

--- client/src/project_banner.js
+++ client/src/project_banner.js
@@ -1,12 +1,13 @@
 import "./project_banner.css"
 import React from 'react';
-import Tag from "./tag.js";
+import Tag from './tag.js';
+import banner from "./static/personal-website-banner.png"
 
 class ProjectBanner extends React.Component {
     render() {
         return <>
             <div class="banner">
-                <img src="personal-website-banner.png" alt="Snippit of Code for this website"/>
+                <img src={banner} alt="Snippit of Code for this website"/>
                 <div class="title">
                     <h1>Sample Project</h1>
                 </div>
