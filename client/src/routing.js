65e0b60d3ea46a3474f3bc47d9f5671f10cd0199
Samuel Steiger <samcsteiger@gmail.com>
2024-07-01 16:28:34 -0400

    Fixed API requests being malformed

--- client/src/routing.js
+++ client/src/routing.js
@@ -4,5 +4,5 @@ export default function getServerURL() {
     if("3000" === window.location.port)
         return "http://localhost:8080/"
     else    
-        return window.location.href
+        return window.location.hostname+"/"
 }
