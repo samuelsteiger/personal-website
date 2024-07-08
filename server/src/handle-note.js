7c848a262aabfa465b2b4e97399eeac94910f161
Samuel Steiger <samcsteiger@gmail.com>
2024-07-08 12:28:07 -0400

    Parametized sql query rather than using string concationation

--- server/src/handle-note.js
+++ server/src/handle-note.js
@@ -2,10 +2,9 @@
 
 const handleNote = async (req, res, pgclient) => {
     pgclient = await pgclient
-    let dbres = await pgclient.query(
-        "INSERT into pw(name, email, body) VALUES(\'"+req.body.name+"\', \'"+req.body.email+"\', \'"+req.body.body+"\');"
-    );
-    console.log(dbres);
+    const text = "INSERT into notes(name, email, body) VALUES($1, $2, $3);"
+    const params = [req.body.name, req.body.email, req.body.text];
+    let dbres = await pgclient.query(text, params);
 };
 
 module.exports = { handleNote }
